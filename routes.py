300c405ccce3fccf6ae2fd8bee37989bcba1be72
Lyubomir Raykov <raykovl@vmware.com>
2015-04-25 14:36:11 +0300

    Adding quotes

--- routes.py
+++ routes.py
@@ -5,22 +5,22 @@ routes = Blueprint('routes', __name__, static_url_path='/static')
 @routes.route('/station', methods=["GET"])
 def get_station():
     # logic
-    return jsonify({answer: 42})
+    return jsonify({'answer': 42})
 
 @routes.route('/station', methods=["POST"])
 def create_station():
     # logic
-    return jsonify({answer: 42})
+    return jsonify({'answer': 42})
 
 @routes.route('/stations', methods=["GET"])
 def get_stations():
     # logic and query params
-    return jsonify({answer: 42})
+    return jsonify({'answer': 42})
 
 @routes.route('/refuel', methods=["POST"])
 def refuel():
     # logic
-    return jsonify({answer: 42})
+    return jsonify({'answer': 42})
 
 @routes.route('/')
 def home():
