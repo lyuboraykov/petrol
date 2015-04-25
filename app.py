5e99d319cb53395cbcdd4ab52ca27f08ce21c18f
Lyubomir Raykov <raykovl@vmware.com>
2015-04-25 14:02:08 +0300

    Table names with underscores

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class UserGasStation(db.Model):
     gas_station_address = db.Column(db.String(80), primary_key=True)
     kilometers = db.Column(db.Float)
     liters = db.Column(db.Float)
-    __table_args__ = (ForeignKeyConstraint([gas_station_city, gas_station_address], ["gas-stations.city", "gas-stations.address"]), {})
+    __table_args__ = (ForeignKeyConstraint([gas_station_city, gas_station_address], ["gas_stations.city", "gas_stations.address"]), {})
 
     def __init__(self, user_id, gas_station_city, gas_station_address):
         self.user_id = user_id
@@ -39,7 +39,7 @@ class UserGasStation(db.Model):
         return '<Id {}>'.format(self.id)
 
 class GasStation(db.Model):
-    __tablename__ = "gas-stations"
+    __tablename__ = "gas_stations"
     city = db.Column(db.String(80), primary_key=True)
     address = db.Column(db.String(80), primary_key=True)
     name = db.Column(db.String(80))
