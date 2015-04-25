2cb73733537e0aa8b68434fb6ee2f4e7aba14042
Lyubomir Raykov <raykovl@vmware.com>
2015-04-25 12:31:27 +0300

    fix imports

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 from flask import render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy, ForeignKey, ForeignKeyConstraints
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
