88b7b7258d28f5b9e836756bcefcc9292b2abc75
Lyubomir Raykov <raykovl@vmware.com>
2015-04-25 12:23:48 +0300

    Putting all models in main file

--- app.py
+++ app.py
@@ -20,3 +20,43 @@ def home():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+class UserGasStation(db.Model):
+    user_id = db.Column(db.String(255), ForeignKey("users.id"), primary_key=True)
+    gas_station_city = db.Column(db.String(80), primary_key=True)
+    gas_station_ad dress = db.Column(db.String(80), primary_key=True)
+    kilometers = db.Column(db.Float)
+    liters = db.Column(db.Float)
+    ForeignKeyConstraints([gas_station_city, gas_station_address], ["GasStation.city", "GasStation.address"])
+
+    def __init__(self, user_id, gas_station_city, gas_station_address):
+        self.user_id = user_id
+        self.gas_station_city = gas_station_city
+        self.gas_station_address = gas_station_address
+
+    def __repr__(self):
+        return '<Id {}>'.format(self.id)
+
+class GasStation(db.Model):
+    city = db.Column(db.String(80), primary_key=True)
+    address = db.Column(db.String(80), primary_key=True)
+    name = db.Column(db.String(80))
+    kilometers = db.Column(db.Float)
+    liters = db.Column(db.Float)
+
+    def __init__(self, city, address, name):
+        self.city = city
+        self.address = address
+        self.name = name
+
+    def __repr__(self):
+        return '<Name: {0}; City: {1}; Address: {2}>'.format(self.name, self.city, self.address)
+
+class User(db.Model):
+    id = db.Column(db.String(255), primary_key=True)
+
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return '<Id {}>'.format(self.id)
