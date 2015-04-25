b0fe54be686eda1fb32ecad1bbe944bf6753881c
Deyan Yanakiev <kliomba@abv.bg>
2015-04-25 17:48:10 +0300

    return json from station route

--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,25 @@
-from flask import jsonify
+import json
 from application import app, db
 from application.models import User, GasStation, UserGasStation
 
 
 @app.route('/station/<city>/<address>', methods=["GET"])
 def get_station(city, address):
-    station = GasStation.query.filter(GasStation.city == city, GasStation.address == address).first()
+    station = GasStation.query \
+        .filter(GasStation.city == city, GasStation.address == address) \
+        .first()
+
     if station is None:
         return None, 404
 
-    return jsonify(station.__dict__), 200
+    return json.dumps({
+        "name": station.name,
+        "address": station.address,
+        "city": station.city,
+        "liters": station.liters,
+        "kilometers": station.kilometers
+    }), 200
+
 
 @app.route('/station/<city>/<address>/<name>', methods=["POST"])
 def create_station(city, address, name):
@@ -17,17 +27,24 @@ def create_station(city, address, name):
     db.session.add(station)
     db.session.commit()
 
-    return jsonify(station.__dict__), 201
+    return json.dumps({
+        "name": station.name,
+        "address": station.address,
+        "city": station.city,
+    }), 201
+
 
 @app.route('/stations', methods=["GET"])
 def get_stations():
     # logic and query params
-    return jsonify({'answer': 42})
+    return json.dumps({'answer': 42})
+
 
 @app.route('/refuel', methods=["POST"])
 def refuel():
     # logic
-    return jsonify({'answer': 42})
+    return json.dumps({'answer': 42})
+
 
 @app.route('/')
 def home():
